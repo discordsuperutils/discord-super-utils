aebfc89690444f33140adb62df23da996aab21d2
adam7100 <73548219+adam757521@users.noreply.github.com>
2021-06-28 04:57:33 +0300

    Update leveling.py

--- examples/leveling.py
+++ examples/leveling.py
@@ -3,7 +3,7 @@ import sqlite3
 from discord.ext import commands
 
 
-database = discordSuperUtils.Database(sqlite3.connect("main.sqlite"))
+database = discordSuperUtils.Database(sqlite3.connect("database"))
 bot = commands.Bot(command_prefix='-')
 LevelingManager = discordSuperUtils.LevelingManager(database, 'xp', bot)
 
@@ -23,4 +23,4 @@ async def rank(ctx):
     member_data = LevelingManager.get_member(ctx.guild, ctx.author)
     await ctx.send(f'You are currently level **{member_data["rank"]}**, with **{member_data["XP"]} XP.')
 
-bot.run("ODExMzMyMDA4Njc2Mjk0NjY3.YCwp0A.z71CpnNpAhom-eLuX4H3DUaaqGQ")
+bot.run("token")
