aef8586dfa761e82c5c6c12a4c20144392902d08
Yash <79107170+koyashie07@users.noreply.github.com>
2021-06-30 08:36:01 -0400

    Add files via upload

---
+++ examples/example.py
@@ -0,0 +1,56 @@
+from discordSuperUtils import MusicManager
+from discord.ext import commands
+
+
+
+bot = commands.Bot(command_prefix='-')
+MusicManager = MusicManager(bot)
+
+
+@bot.event
+async def on_ready():
+    print('Music manager is ready.', bot.user)
+
+
+
+@bot.command()
+async def leave(ctx):
+    if await MusicManager.leave(ctx):
+        await ctx.send("Left Voice Channel Lol Gang Shit")
+
+
+@bot.command()
+async def np(ctx):
+    if player := await MusicManager.now_playing(ctx):
+        await ctx.send(f"Currently playing: {player}")
+
+
+@bot.command()
+async def join(ctx):
+    if await MusicManager.join(ctx):
+        await ctx.send("Joined Voice Channel Lol Gang Shit!")
+
+
+@bot.command()
+async def play(ctx, *, query: str):
+    player = await MusicManager.create_player(query)
+    await MusicManager.queue_add(player=player, ctx=ctx)
+
+    if not await MusicManager.play(ctx):
+        await ctx.send("Added to queue")
+
+
+@bot.command()
+async def volume(ctx, volume: int):
+    await MusicManager.volume(ctx, volume)
+
+@bot.command()
+async def loop(ctx):
+    is_loop = await MusicManager.loop(ctx)
+    await ctx.send(f"Looping toggled to {is_loop}")
+
+@bot.command()
+async def stop(ctx):
+    ctx.voice_client.stop()
+
+bot.run("token")
