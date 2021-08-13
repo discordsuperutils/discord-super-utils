2110a524d6bea9264062ebba2f527ec50a9bcbde
adam757521 <73548219+adam757521@users.noreply.github.com>
2021-08-13 05:29:46 +0300

    fixed database example

--- examples/database.py
+++ examples/database.py
@@ -2,7 +2,7 @@ import discordSuperUtils
 import pymongo
 import sqlite3
 
-mongo_database = discordSuperUtils.DatabaseManager(pymongo.MongoClient("connection string").test)
+mongo_database = discordSuperUtils.DatabaseManager(pymongo.MongoClient("connection string")["DATABASENAME"])
 sqlite_database = discordSuperUtils.DatabaseManager(sqlite3.connect("database"))
 
 values = sqlite_database.insert({"guild": ..., "member": ...}, "table")
