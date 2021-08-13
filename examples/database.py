00dee69e9fa65d2e0086b694a1f3cd4a7e692efb
adam757521 <73548219+adam757521@users.noreply.github.com>
2021-08-13 05:28:23 +0300

    added database example

---
+++ examples/database.py
@@ -0,0 +1,9 @@
+import discordSuperUtils
+import pymongo
+import sqlite3
+
+mongo_database = discordSuperUtils.DatabaseManager(pymongo.MongoClient("connection string").test)
+sqlite_database = discordSuperUtils.DatabaseManager(sqlite3.connect("database"))
+
+values = sqlite_database.insert({"guild": ..., "member": ...}, "table")
+print(values)
