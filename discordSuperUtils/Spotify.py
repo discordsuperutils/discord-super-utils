54e09241d2f606155ed873f31abda9fba47f3e5d
adam757521 <73548219+adam757521@users.noreply.github.com>
2021-08-28 05:59:24 +0300

    added fetch_full_playlist fixed spotify url type parsing fixed blocking

--- discordSuperUtils/Spotify.py
+++ discordSuperUtils/Spotify.py
@@ -2,6 +2,8 @@ from spotipy import SpotifyClientCredentials
 import spotipy
 import asyncio
 
+FIELD = "items.track.name,items.track.artists(name),total"
+
 
 class Spotify:
     def __init__(self, client_id, client_secret):
@@ -10,31 +12,47 @@ class Spotify:
         self.loop = asyncio.get_event_loop()
 
     @staticmethod
-    async def get_type(url):
+    def get_type(url):
         url = url.replace("https://open.spotify.com/", "")
-        return url.split("/")[0]
+        return url.split("/")[-2]
 
     @staticmethod
-    async def make_title(song):
+    def make_title(song):
         artists = " ".join([artist['name'] for artist in song.get('artists')])
         return f"{song['name']} by {artists}"
 
+    async def fetch_playlist_data(self, url, offset):
+        return await self.loop.run_in_executor(None, lambda: self.sp.playlist_items(playlist_id=url,
+                                                                                    fields=FIELD,
+                                                                                    offset=offset))
+
+    async def fetch_full_playlist(self, url):
+        initial_request = await self.fetch_playlist_data(url, 0)
+        total_tracks = initial_request.get('total')
+
+        requests = list(
+            await asyncio.gather(*(self.fetch_playlist_data(url, offset) for offset in range(100, total_tracks, 100)))
+        )
+        requests.insert(0, initial_request)
+        result_tracks = []
+
+        for request in requests:
+            result_tracks += request.get('items')
+
+        return result_tracks
+
     async def get_songs(self, url):
-        playlist_type = await self.get_type(url)
-        songs = None
+        playlist_type = self.get_type(url)
+        songs = []
 
         if playlist_type == "playlist":
-            playlist = await self.loop.run_in_executor(None, self.sp.playlist_items(playlist_id=url,
-                                                                                    fields='items.track.name,items.track.artists(name)',
-                                                                                    offset=0))
-            playlist = playlist.get('items')
-            return [await self.make_title(song['track']) for song in playlist]
+            return [self.make_title(song['track']) for song in await self.fetch_full_playlist(url)]
 
-        elif playlist_type == "track":
-            songs = [await self.loop.run_in_executor(None, self.sp.track(track_id=url))]
+        if playlist_type == "track":
+            songs = [await self.loop.run_in_executor(None, lambda: self.sp.track(track_id=url))]
 
-        elif playlist_type == "album":
-            album = await self.loop.run_in_executor(None, self.sp.album_tracks(album_id=url, limit=50))
+        if playlist_type == "album":
+            album = await self.loop.run_in_executor(None, lambda: self.sp.album_tracks(album_id=url, limit=50))
             songs = album.get("items")
 
-        return [await self.make_title(song) for song in songs]
+        return [self.make_title(song) for song in songs]
