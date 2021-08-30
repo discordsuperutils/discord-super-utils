4484d753d41ac8345892cf72cae34c9d30b6c1b5
adam7100 <73548219+adam757521@users.noreply.github.com>
2021-08-30 22:27:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ f = open("README.md", "r")
 README = f.read()
 
 setup(
-    name='discordSuperUtils',
-    packages=['discordSuperUtils'],
+    name='discordSuperUtils-splitted',
+    packages=['discordSuperUtils-splitted'],
     version='0.1.4',
     license='MIT',
     description='Discord Bot Development made easy!',
     long_description=README,
     long_description_content_type="text/markdown",
-    author='koyashie07',
-    url='https://github.com/discordsuperutils/discord-super-utils',
-    download_url='https://github.com/discordsuperutils/discord-super-utils/archive/refs/tags/v0.1.4.tar.gz',
+    author='adam7100',
+    url='https://github.com/adam757521/discordSuperUtils-splitted/',
+    download_url='https://github.com/adam757521/discordSuperUtils-splitted/archive/refs/tags/v0.0.1.tar.gz',
     keywords=['discord', 'easy', 'discord.py', 'music', 'download', 'links', 'images', 'videos', 'audio', 'bot',
               'paginator', 'economy', 'reaction', 'reaction roles', 'database', 'database manager'],
     install_requires=[
